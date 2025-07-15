82a5dba637609f4d343ac9d9f90cd66cd85d6144
chorny-znich <vargar.dt@gmail.com>
2025-07-15 11:39:14 +0300

    feat: add two types of buttons - a text button and a image button

---
+++ image_button.cpp
@@ -0,0 +1,22 @@
+#include "image_button.h"
+#include "engine_data.h"
+
+namespace dr {
+  ImageButton::ImageButton(sf::Vector2f size, const std::string& id) :
+    Button(size)
+  {
+    mImage.setTexture(Textures::get(id));
+  }
+  void ImageButton::setPosition(sf::Vector2f pos)
+  {
+    mImage.setPosition(pos);
+  }
+  void ImageButton::render(sf::RenderWindow& window)
+  {
+    window.draw(mImage);
+  }
+  bool ImageButton::isClicked(sf::Vector2f pos)
+  {
+    return (mImage.getGlobalBounds().contains(pos)) ? true : false;
+  }
+}

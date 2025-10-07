938ef3df988abc36ef4926fd8ad4c18840fca696
chorny-znich <vargar.dt@gmail.com>
2025-10-07 08:27:35 +0300

    feat: add the map manager

---
+++ map_manager.cpp
@@ -0,0 +1,16 @@
+#include "map_manager.h"
+#include <format>
+
+dr::MapManager::MapManager(size_t numberOfMaps) :
+	NUMBER_OF_MAPS{numberOfMaps}
+{
+}
+
+void dr::MapManager::init()
+{
+    for (size_t i{ 0 }; i < NUMBER_OF_MAPS; i++) {
+        std::shared_ptr<Map> newMap = std::make_shared<Map>();
+        newMap->loadFromFile(std::format("data/maps/map_{}.ini", i));
+        mMaps.insert({ i, std::move(newMap) });
+    }
+}
---
+++ map_manager.h
@@ -0,0 +1,28 @@
+#pragma once
+#include "map.h"
+#include <memory>
+#include <unordered_map>
+#include <SFML/System.hpp>
+
+namespace dr
+{
+	class MapManager
+	{
+	private:
+		const size_t NUMBER_OF_MAPS = 1;
+		const sf::Vector2f TILE_SIZE{ 128.f, 128.f };
+		size_t mCurrentMapIndex{ 0 };
+		std::unordered_map<size_t, std::shared_ptr<Map>> mMaps;
+		//std::map <size_t, bool> mVisitedMaps;
+	public:
+		MapManager(size_t numberOfMaps);
+		void init();
+		std::shared_ptr<Map> getCurrentMap();
+		//void setPlayerSpawnPosition(sf::Vector2u pos);
+		//sf::Vector2u getPlayerSpawnPosition();
+		void setCurrentMapIndex(size_t index);
+		/*
+		void setVisited();
+		bool isVisited() const;*/
+	};
+}

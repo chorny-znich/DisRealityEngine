829d4cfca2bd750aee2f5e6f608bc6e69144b14e
chorny-znich <vargar.dt@gmail.com>
2026-06-07 23:18:59 +0300

    feat: add abstract class for game screens

---
+++ screen.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+
+/**
+ * @brief Abstract class for the game screens
+ */
+class Screen
+{
+public:
+	virtual void init() = 0;
+	virtual void handleInput() = 0;
+	virtual void update() = 0;
+	virtual void render(sf::RenderWindow& window) = 0;
+};
+

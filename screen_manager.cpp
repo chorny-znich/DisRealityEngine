e684541983459fe20520413c462a4d609d7bb833
chorny-znich <vargar.dt@gmail.com>
2026-06-08 17:01:50 +0300

    feat: add proper namespace for ScreenManager

--- screen_manager.cpp
+++ screen_manager.cpp
@@ -1,36 +1,39 @@
 #include "screen_manager.h"
 #include <assert.h>
 
-ScreenManager::ScreenManager() {
-	assert(mpManager == nullptr);
-	mpManager = this;
-}
-
-/**
- * @brief Pop game screen from stack
-*/
-void ScreenManager::destroyScreen() {
-	auto& screens = mpManager->mScreens;
-	screens.pop();
-}
-
-/**
- * @brief Get active game screen
- * @return shared_ptr to active game screen
-*/
-std::shared_ptr<Screen> ScreenManager::getCurrent()
+namespace dr
 {
-	auto& screens = mpManager->mScreens;
-	assert(!screens.empty());
+	ScreenManager::ScreenManager() {
+		assert(mpManager == nullptr);
+		mpManager = this;
+	}
 
-	return screens.top().second;
-}
+	/**
+	 * @brief Pop game screen from stack
+	*/
+	void ScreenManager::destroyScreen() {
+		auto& screens = mpManager->mScreens;
+		screens.pop();
+	}
 
-/**
- * @brief Check if the stack of game screens empty
- * @return Are there any game screens in the stack
-*/
-bool ScreenManager::isEmpty()
-{
-	return mScreens.empty();
+	/**
+	 * @brief Get active game screen
+	 * @return shared_ptr to active game screen
+	*/
+	std::shared_ptr<Screen> ScreenManager::getCurrent()
+	{
+		auto& screens = mpManager->mScreens;
+		assert(!screens.empty());
+
+		return screens.top().second;
+	}
+
+	/**
+	 * @brief Check if the stack of game screens empty
+	 * @return Are there any game screens in the stack
+	*/
+	bool ScreenManager::isEmpty()
+	{
+		return mScreens.empty();
+	}
 }

515248f8592a56783fdf325812518fc724430685
chorny-znich <vargar.dt@gmail.com>
2026-08-19 09:54:47 +0300

    feat: add image button

--- image_button.cpp
+++ image_button.cpp
@@ -8,10 +8,67 @@ namespace dr
    * @param size 
    * @param sprite 
    */
-  dr::ImageButton::ImageButton(sf::Vector2f size, uint16_t spriteID) :
+  dr::ImageButton::ImageButton(sf::Vector2f size, uint16_t spriteID, uint16_t overlapSpriteID) :
     Button(size),
-    mSprite(dr::SpriteDatabase::instance().getSprite(spriteID))
+    mSprite(dr::SpriteDatabase::instance().getSprite(spriteID)),
+    mOverlapSprite(dr::SpriteDatabase::instance().getSprite(overlapSpriteID)),
+    mCurrentSprite(mSprite)
   {
     //mSprite = std::make_unique<sf::Sprite>(dr::SpriteDatabase::instance().getSprite(spriteID));
+  }
+
+  /**
+   * @brief Set button's position on the screen as sf::Vector2f
+   * @param pos pair of float coordinates
+   */
+  void ImageButton::setPosition(sf::Vector2f pos)
+  {
+    mCurrentSprite.setPosition(pos);
+  }
+
+  /**
+   * @brief 
+   * @param pos 
+   * @return 
+   */
+  bool ImageButton::isOverlap(sf::Vector2f pos)
+  {
+    bool isCurrentOverlap = mIsOverlap;
+    if (mCurrentSprite.getGlobalBounds().contains(pos))
+    {
+      mIsOverlap = true;
+      if (mIsOverlap != isCurrentOverlap)
+      {
+        mCurrentSprite = mOverlapSprite;
+      }
+      return true;
+    }
+
+    mIsOverlap = false;
+    if (mIsOverlap != isCurrentOverlap)
+    {
+      mCurrentSprite = mSprite;
+    }
+
+    return false;
+  }
+
+  /**
+   * @brief 
+   * @param pos 
+   * @return 
+   */
+  bool ImageButton::isClicked(sf::Vector2f pos)
+  {
+    return (mCurrentSprite.getGlobalBounds().contains(pos)) ? true : false;
+  }
+
+  /**
+   * @brief Draw button's sprite on the screen
+   * @param window Reference to sf::RenderWindow which draw button's sprite 
+   */
+  void ImageButton::render(sf::RenderWindow& window)
+  {
+    window.draw(mCurrentSprite);
   }
 }
--- image_button.h
+++ image_button.h
@@ -9,9 +9,17 @@ namespace dr
   class ImageButton : public Button
   {
   public:
-    ImageButton(sf::Vector2f size, uint16_t spriteID);
+    ImageButton(sf::Vector2f size, uint16_t spriteID, uint16_t overlapSpriteID);
+    void setPosition(sf::Vector2f pos) override;
+
+    virtual bool isOverlap(sf::Vector2f pos) override;
+    virtual bool isClicked(sf::Vector2f pos) override;
+    virtual void render(sf::RenderWindow& window) override;
   private:
     //std::unique_ptr<sf::Sprite> mSprite{ nullptr };
     sf::Sprite mSprite;
+    sf::Sprite mOverlapSprite;
+    sf::Sprite mCurrentSprite;
+    bool mIsOverlap{ false };
   };
 }
